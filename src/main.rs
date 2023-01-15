ae664c4bb3acff091b86616f15a09c04917ff7a6
Soya Ohnishi <soya.ohnishi@gmail.com>
2023-01-15 20:25:06 +0900

    Add implementation

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
-use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::{digital::v2::OutputPin, PwmPin};
 use panic_probe as _;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
-    pac,
+    entry, pac,
     sio::Sio,
     watchdog::Watchdog,
 };
+
+const LOW: u16 = 0;
+const HIGH: u16 = 25000;
 
 #[entry]
 fn main() -> ! {
@@ -31,9 +27,8 @@ fn main() -> ! {
     let sio = Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
     let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+        bsp::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -53,15 +48,26 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut led_pin = pins.led.into_push_pull_output();
+    let led_pin = pins.led.into_push_pull_output();
+
+    let mut pwm_slices = bsp::hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+    let pwm = &mut pwm_slices.pwm4;
+    pwm.set_ph_correct();
+    pwm.enable();
+
+    let channel = &mut pwm.channel_b;
+    channel.output_to(led_pin);
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        for i in LOW..=HIGH {
+            delay.delay_us(20);
+            channel.set_duty(i);
+        }
+
+        for i in (LOW..=HIGH).rev() {
+            delay.delay_us(20);
+            channel.set_duty(i);
+        }
     }
 }
 
